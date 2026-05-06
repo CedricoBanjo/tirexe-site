5e429784ae8e2bb285988685fc6d2d5e8a96e742
CedricoBanjo <femmininocedric@yahoo.fr>
2026-05-06 12:20:43 +0200

    Create sitemap.js to generate blog sitemap
    This file generates a sitemap for the blog, including URLs for articles and other pages.

---
+++ sitemap.js
@@ -0,0 +1,34 @@
+import { getAllArticles } from '../lib/blog'
+
+export default function sitemap() {
+  const articles = getAllArticles()
+
+  const articleUrls = articles.map(article => ({
+    url: `https://tirexe.com/blog/${article.slug}`,
+    lastModified: new Date(article.date),
+    changeFrequency: 'monthly',
+    priority: 0.7,
+  }))
+
+  return [
+    {
+      url: 'https://tirexe.com',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1.0,
+    },
+    {
+      url: 'https://tirexe.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://tirexe.com/mentions-legales',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+    ...articleUrls,
+  ]
+}
